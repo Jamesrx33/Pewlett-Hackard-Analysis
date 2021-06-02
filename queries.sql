77666e42944ea39ec2d6ae19182949f99f792dae
Jamesrx33 <68870179+Jamesrx33@users.noreply.github.com>
2021-06-02 08:01:48 -0500

    7.3.1 commit

---
+++ queries.sql
@@ -0,0 +1,63 @@
+SELECT * FROM employees;
+
+-- Select All retirement aged Employees
+SELECT first_name, last_name
+FROM 
+	employees
+WHERE 
+	birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Select 1952 BD from Employees
+SELECT first_name, last_name
+FROM 
+	employees
+WHERE 
+	birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Select 1953 BD from Employees
+SELECT first_name, last_name
+FROM 
+	employees
+WHERE 
+	birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Select 1954 BD from Employees
+SELECT first_name, last_name
+FROM 
+	employees
+WHERE 
+	birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- Select 1955 BD from Employees
+SELECT first_name, last_name
+FROM 
+	employees
+WHERE 
+	birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Select All retirement aged Employees (Narrowed by hire date)
+SELECT first_name, last_name
+FROM 
+	employees
+WHERE 
+	(birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Count selected rows from employee Table
+SELECT COUNT (first_name)
+FROM 
+	employees
+WHERE 
+	(birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
